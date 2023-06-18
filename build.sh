8c233f2f6dd1a308ba69ba48700140f90e5d1545
Andreas Huber <andreas@andreashuber.no>
2023-06-18 10:28:13 +0200

    Download dependencies directly

---
+++ build.sh
@@ -0,0 +1,5 @@
+export BOOST_ROOT=/tmp/boost
+export LD_LIBRARY_PATH=$BOOST_ROOT/lib:$LD_LIBRARY_PATH
+
+cmake -DCMAKE_BUILD_TYPE=RELEASE .
+make
---
+++ install-dependencies.sh
@@ -0,0 +1,21 @@
+if [[ -d /tmp/boost ]] 
+then
+    echo "Boost already installed"
+else
+    mkdir -p /tmp/boost
+    wget -c https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.tar.gz -O - | tar -xz -C /tmp/boost
+    /tmp/boost/boost_1_82_0/bootstrap.sh --prefix=/tmp/boost
+    /tmp/boost/boost_1_82_0/b2 install
+fi
+
+
+if [[ -d inc/eigen3 ]] 
+then
+    echo "Eigen3 already installed"
+else
+    rm -r inc/eigen3
+    rm -r /tmp/eigen3
+    mkdir -p /tmp/eigen3
+    wget -c https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz -O - | tar -xz -C /tmp/eigen3
+    mv /tmp/eigen3/eigen-3.4.0/ inc/Eigen3
+fi
